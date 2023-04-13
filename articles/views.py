3e92d5d585ca91ef564ab90749d085d8e643986e
Lightness-of-Being <gje960217@naver.com>
2023-04-13 16:25:21 +0900

    create 작성

--- articles/views.py
+++ articles/views.py
@@ -27,3 +27,18 @@ def detail(request, review_pk):
     }
     return render(request, 'articles/detail.html', context)
 
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/create.html', context)
