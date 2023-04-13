c235ce76764de109a4a359ba2644392042de4d84
Lightness-of-Being <gje960217@naver.com>
2023-04-13 14:49:18 +0900

    1. urls 수정해서 바로 articles index 로 가게 하기 2. index views함수 완성 3. 임시 index.html 작성

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def init(request):
+    return redirect('articles:index')
+
+def index(request):
+    context = {
+
+    }
+    return render(request, 'articles/index.html', context)
