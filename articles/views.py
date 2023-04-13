f2b5b5d9516e360962134824dac1221d3a3f62e7
Lightness-of-Being <gje960217@naver.com>
2023-04-13 16:22:39 +0900

    detail 작성

--- articles/views.py
+++ articles/views.py
@@ -10,7 +10,20 @@ def init(request):
     return redirect('articles:index')
 
 def index(request):
+    articles = Article.objects.all()
     context = {
-
+        'articles': articles,
     }
     return render(request, 'articles/index.html', context)
+
+def detail(request, review_pk):
+    article = Article.objects.get(pk=review_pk)
+    comment_form = CommentForm()
+    comments = article.comment_set.all()
+    context = {
+        'article': article,
+        'comment_form': comment_form,
+        'comments': comments,
+    }
+    return render(request, 'articles/detail.html', context)
+
