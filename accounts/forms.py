43fc80cf78a227b6f0eb4f0d335e01307b62b5cf
fanta95 <fantael95@gmail.com>
2023-04-14 11:10:55 +0900

    forms.py customuserchangeform 추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,3 +15,8 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = get_user_model()
+        fields = ('email','first_name','last_name')
